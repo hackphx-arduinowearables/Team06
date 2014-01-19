f7c5123e6fa6a22bc43eec27a01435e0fc2a4a59
Vivie Truong <vntruong@asu.edu>
2014-01-18 17:19:44 -0700

    Added other images for LEDMatrix

--- code/CombineLEDSketch/CombineLEDSketch.ino
+++ code/CombineLEDSketch/CombineLEDSketch.ino
@@ -46,6 +46,7 @@ void loop() {
 //  colorWipe(strip.Color(0, 0, 255), 50); // Blue
   colorWipe(strip.Color(255, 255, 0), 50); // Probably Yellow.
   
+  // Caution Triangle
   fiveBySeven.setPoint(2, 5, 1);
     delay(100);
     fiveBySeven.setPoint(3, 4, 1);
@@ -65,7 +66,73 @@ void loop() {
     fiveBySeven.setPoint(0, 2, 1);
     delay(100);
     fiveBySeven.setPoint(0, 3, 1);
-    delay(1000);
+    delay(100);
+    
+    // Safe Smiley-face
+    
+//    fiveBySeven.setPoint(4, 2, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(3, 1, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(2, 1, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(1, 1, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(0, 2, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(3, 4, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(1, 4, 1);
+//    delay(100);
+
+    // Good Checkmark
+
+//   fiveBySeven.setPoint(0, 5, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(1, 4, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(3, 2, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(4, 3, 1);
+//    delay(100);
+//    delay(100);
+
+    //Bad Skull
+    
+//    fiveBySeven.setPoint(1, 0, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(2, 0, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(3, 0, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(0, 1, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(2, 1, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(4, 1, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(0, 2, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(2, 2, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(4, 2, 1);
+//    delay(100);
+//     fiveBySeven.setPoint(2, 3, 1);
+//    delay(100);
+//     fiveBySeven.setPoint(0, 5, 1);
+//    delay(100);
+//     fiveBySeven.setPoint(1, 5, 1);
+//    delay(100);
+//     fiveBySeven.setPoint(2, 5, 1);
+//    delay(100);
+//     fiveBySeven.setPoint(3, 5, 1);
+//    delay(100);
+//     fiveBySeven.setPoint(4, 5, 1);
+//    delay(100);
+//     fiveBySeven.setPoint(0, 6, 1);
+//    delay(100);
+//    fiveBySeven.setPoint(0, 4, 1);
+//    delay(100);
 }
 
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -75,79 +142,3 @@ void colorWipe(uint32_t c, uint8_t wait) {
       delay(wait);
   }
 }
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-
